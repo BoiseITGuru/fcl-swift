750793ad587bd9acefefd625481656f0f9f62190
BoiseITGuru <dev@boiseitguru.com>
2022-09-06 04:16:54 -0600

    bump flow-swift version

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/outblock/flow-swift.git", .exact("0.2.2")),
+        .package(url: "https://github.com/outblock/flow-swift.git", .exact("0.2.4")),
     ],
     targets: [
         .target(
